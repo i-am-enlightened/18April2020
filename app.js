a95bcc3829075d1b9cab9a9f1b41026ee10289f5
i-am-enlightened <31614408+i-am-enlightened@users.noreply.github.com>
2020-04-18 13:52:47 +0530

    Ticket (#1)
    * 1.txt in ticket
    
    * app.js in ticket

---
+++ app.js
@@ -0,0 +1,17 @@
+function add(a,b){
+  return a+b
+}
+
+function subtract(a,b){
+  return a-b
+}
+
+function multiply(a,b){
+  return a*b
+}
+
+var x = add(10,5)
+var y = subtract(10,5)
+var z = multiply(10,5)
+
+console.log(x+" "+y+" "+z)
